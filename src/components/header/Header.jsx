b85f71111e74400fb43b7b451b914fc8aa7680d4
UniconioIn <natalia@unicornioindustries.co>
2024-01-26 23:30:17 -0500

    First structure

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function Header() {
+
+
+  return (
+    <>
+        <img src="./src/assets/payqubit.png" alt="" />
+        <h1>Hello world</h1>
+    </>
+  )
+}
+
+export default Header
